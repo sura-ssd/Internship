3a3770ed54b11c1665c79d7026e6b7407ef00ff2
sura <surashiferaw564@gmail.com>
2023-06-01 12:23:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 function App() {
+  var [list, setlist] = useState([])
+
+  useEffect(()=>{
+
+     axios.get('https://android-kotlin-fun-mars-server.appspot.com/amphibians')
+  .then(function (response) {
+    // handle success
+    setlist(response.data)
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+  },[])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+
+      {
+      list.map((data)=>{
+      return <div >
+       <div class="img"><img  src={data?.img_src}/></div> 
+        <p >{data?.name}</p>
+        <p id="desc">{data?.description}</p>
+        <h1>{data?.type}</h1>
+        </div>
+      })
+      }
     </div>
   );
 }
